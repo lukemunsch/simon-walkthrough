9fc20390693d268ab9483bea6d08813f7db787dc
lukemunsch <lpmunsch@gmail.com>
2021-12-16 11:24:28 +0000

    add content with new test to test file

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,31 +2,45 @@
  * @jest-environment jsdom
  */
 
- const { game, newGame, showScore } = require("../game");
+const {
+    game,
+    newGame,
+    showScore
+} = require("../game");
 
 
- beforeAll(() => {
-     let fs = require("fs");
-     let fileContents = fs.readFileSync("index.html", "utf-8");
-     document.open();
-     document.write(fileContents);
-     document.close();
- });
- 
- describe("game object contains correct keys", () => {
-     test("score key exists", () => {
-         expect("score" in game).toBe(true);
-     });
-     test("currentGame key exists", () => {
-         expect("currentGame" in game).toBe(true);
-     });
-     test("playerMoves key exists", () => {
-         expect("playerMoves" in game).toBe(true);
-     });
-     test("choices key exists", () => {
-         expect("choices" in game).toBe(true);
-     });
-     test("choices contain correct ids", () => {
-         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
-     });
- });
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("new game function works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("game score should be set to 0", () => {
+        expect("score" in game).toEqual(0);
+    })
+})
